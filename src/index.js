dd751dbf4385badd1c6e315d84093003fd644612
andres-condezo <ra.condezomonge@gmail.com>
2022-01-14 10:20:53 -0500

    Feat(src/index): [Update the index.js file with the main functions]

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
-import './style.css';
-import { createElement, render } from './create-element';
-import taskArr from './task-arr';
+// *****************
+// Imports
+// *****************
 
-const displayTasks = (taskArr) => {
-  taskArr.forEach((task) => {
-    const input = createElement('input', {
-      type: 'checkBox',
-      class: 'check-box',
-    });
-    const textTask = task.description;
-    const icon = createElement('i', { class: 'fas fa-ellipsis-v kebab' });
-    const li = createElement('li', { class: 'task-li' }, [input, textTask, icon]);
-    render(li, document.getElementById('root'));
+import './styles/style.css';
+import ToDoApp from './modules/app.js';
+import { $ } from './modules/utils.js';
+
+// ***************
+//  Main Function
+// ***************
+
+const app = new ToDoApp();
+const $newTaskInput = $('.new-task');
+const $refreshBtn = $('.refresh');
+
+const main = () => {
+  app.getLocalStorage();
+  app.displayTasks();
+  $refreshBtn.addEventListener('click', () => { app.displayTasks(); });
+  $newTaskInput.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+      app.addTask($newTaskInput);
+    }
   });
 };
 
-displayTasks(taskArr);
+main();
