068aae2dfbab7280a9f4ea1fb9fb9e32555ec735
andres-condezo <ra.condezomonge@gmail.com>
2022-01-18 18:57:52 -0500

    Refactor(task.js): [Delete default properties]

--- src/modules/task.js
+++ src/modules/task.js
@@ -1,5 +1,5 @@
 class Task {
-  constructor(description = 'All clean', completed = false, index = 0) {
+  constructor(description = '', completed = false, index = 0) {
     this.description = description;
     this.completed = completed;
     this.index = index;
